5bf7db150924a6e0ea6ff1b774a7cc706c33da12
Admin1 <schneider_v_s@mail.ru>
2022-06-30 19:01:28 +0300

    Create test onUpdateReceived

---
+++ tg-bot/src/test/java/com/tutrit/tgbot/example/service/TelegramBotTest.java
@@ -0,0 +1,12 @@
+package com.tutrit.tgbot.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TelegramBotTest {
+
+    @Test
+    void onUpdateReceived() {
+    }
+}
