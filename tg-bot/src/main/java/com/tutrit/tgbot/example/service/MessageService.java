246c932cdf73dc916e04ceb27a64d611ac5d27df
Yury Savitski <yurasava@gmail.com>
2022-07-03 15:10:28 +0300

    add MessageService.java

---
+++ tg-bot/src/main/java/com/tutrit/tgbot/example/service/MessageService.java
@@ -0,0 +1,33 @@
+package com.tutrit.tgbot.example.service;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class MessageService {
+
+	public SendMessage onUpdateReceived(Update update) {
+		SendMessage sendMessage = new SendMessage();
+		if(update != null) {
+			Message message = update.getMessage();
+			sendMessage.setChatId(String.valueOf(message.getChatId()));
+			if(message != null && message.hasText()) {
+				String magText = message.getText();
+				if(magText.equals("/start")) {
+					sendMessage.setText("Hello!");
+					return sendMessage;
+				} else if(magText.equals("/settings")) {
+					sendMessage.setText("Settings!");
+					return sendMessage;
+				} else if(magText.equals("/help")) {
+					sendMessage.setText("Help!");
+					return sendMessage;
+				}
+			}
+		}
+		sendMessage.setText("Do no");
+		return sendMessage;
+	}
+}
