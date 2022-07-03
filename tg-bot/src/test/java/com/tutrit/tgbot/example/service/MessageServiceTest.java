3e5a5ba1a363eca7f64b0eb0c0c0705e288096a1
Yury Savitski <yurasava@gmail.com>
2022-07-03 18:39:53 +0300

    add MessageServiceTest.java

---
+++ tg-bot/src/test/java/com/tutrit/tgbot/example/service/MessageServiceTest.java
@@ -0,0 +1,65 @@
+package com.tutrit.tgbot.example.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutrit.tgbot.example.config.Mapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = {TelegramBot.class, Mapper.class, MessageService.class})
+class MessageServiceTest {
+
+	@Autowired
+	TelegramBot telegramBot;
+	@Autowired
+	ObjectMapper objectMapper;
+	@Autowired
+	MessageService messageService;
+
+
+	@Test
+	void onUnknownReceived() throws IOException {
+		Update update = objectMapper.readValue(new File("src/test/resources/update.json"), Update.class);
+		SendMessage actualResult = messageService.onUpdateReceived(update);
+		SendMessage expectedResult = makeMessage("Do no");
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	void onStartReceived() throws IOException {
+		Update update = objectMapper.readValue(new File("src/test/resources/start.json"), Update.class);
+		SendMessage actualResult = messageService.onUpdateReceived(update);
+		SendMessage expectedResult = makeMessage("Hello!");
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	void onSettingsReceived() throws IOException {
+		Update update = objectMapper.readValue(new File("src/test/resources/settings.json"), Update.class);
+		SendMessage actualResult = messageService.onUpdateReceived(update);
+		SendMessage expectedResult = makeMessage("Settings!");
+		assertEquals(expectedResult, actualResult);
+	}
+
+	@Test
+	void onHelpReceived() throws IOException {
+		Update update = objectMapper.readValue(new File("src/test/resources/help.json"), Update.class);
+		SendMessage actualResult = messageService.onUpdateReceived(update);
+		SendMessage expectedResult = makeMessage("Help!");
+		assertEquals(expectedResult, actualResult);
+	}
+
+	private SendMessage makeMessage(String text) {
+		SendMessage sendMessage = new SendMessage();
+		sendMessage.setChatId(453309853L);
+		sendMessage.setText(text);
+		return sendMessage;
+	}
+}
