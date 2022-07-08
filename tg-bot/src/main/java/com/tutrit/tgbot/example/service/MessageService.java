6db0d05294524368c66d0f872146f3e750c845fd
Yury Savitski <yurasava@gmail.com>
2022-07-08 15:56:30 +0300

    update MessageService.java

--- tg-bot/src/main/java/com/tutrit/tgbot/example/service/MessageService.java
+++ tg-bot/src/main/java/com/tutrit/tgbot/example/service/MessageService.java
@@ -5,23 +5,29 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import static com.tutrit.tgbot.example.constants.ChatBotCommands.GET_ORDER_STATUS;
+import static com.tutrit.tgbot.example.constants.ChatBotCommands.START;
+
 @Service
 public class MessageService {
 
 	public SendMessage onUpdateReceived(Update update) {
 		SendMessage sendMessage = new SendMessage();
-		if(update != null) {
+		if (update != null) {
 			Message message = update.getMessage();
 			sendMessage.setChatId(message.getChatId());
-			if(message != null && message.hasText()) {
+			if (message != null && message.hasText()) {
 				String magText = message.getText();
-				if(magText.equals("/start")) {
+				if (magText.equals(START.getCommand())) {
 					sendMessage.setText("Hello!");
 					return sendMessage;
-				} else if(magText.equals("/settings")) {
+				} else if (magText.equals(GET_ORDER_STATUS.getCommand())) {
+					sendMessage.setText("Get order status!");
+					return sendMessage;
+				} else if (magText.equals("/settings")) {
 					sendMessage.setText("Settings!");
 					return sendMessage;
-				} else if(magText.equals("/help")) {
+				} else if (magText.equals("/help")) {
 					sendMessage.setText("Help!");
 					return sendMessage;
 				}
