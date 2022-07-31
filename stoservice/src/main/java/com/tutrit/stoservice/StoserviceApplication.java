a7433f9ddb27f569ffb6d633e35e9dfe6f32ac40
yurasava <yurasava@gmail.com>
2022-07-31 20:41:31 +0300

    add stoservice module

---
+++ stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tutrit.stoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoserviceApplication.class, args);
+    }
+
+}
