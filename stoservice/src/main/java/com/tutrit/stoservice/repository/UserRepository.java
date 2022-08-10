4afd035bbdce151958acbb825978cdcec94fc0fd
lamaconda <kennethoffi6.6@gmail.com>
2022-08-10 17:59:53 +0400

    TGBOT-14 adds CRUD methods and tests. Didn't figure out how to write test for findUser method. In update user I can change id of user and cant change key, so we must block possibility of changing id in user.

--- stoservice/src/main/java/com/tutrit/stoservice/repository/UserRepository.java
+++ stoservice/src/main/java/com/tutrit/stoservice/repository/UserRepository.java
@@ -1,19 +1,28 @@
 package com.tutrit.stoservice.repository;
 
 import com.tutrit.stoservice.bean.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class UserRepository {
 
-    public static Logger logger = LoggerFactory.getLogger(UserRepository.class);
 
     public static Map<String, User> userMap = new HashMap<>();
 
     public void createUser(String id, User user) {
         userMap.put(id, user);
+    }
+
+    public User findUser(String id) {
+        return userMap.get(id);
+    }
+
+    public User updateUser(String id, User user) {
+        return userMap.replace(id, user);
+    }
+
+    public User deleteUser(String id) {
+        return userMap.remove(id);
     }
 }
--- stoservice/src/test/java/com/tutrit/stoservice/repository/UserRepositoryTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/repository/UserRepositoryTest.java
@@ -13,14 +13,35 @@ class UserRepositoryTest {
     @BeforeEach
     void SetUp() {
         userRepository = new UserRepository();
+        UserRepository.userMap.put("1", new User("1", "user1", "phone1"));
+        UserRepository.userMap.put("2", new User("2", "user2", "phone2"));
+        UserRepository.userMap.put("3", new User("3", "user3", "phone3"));
         UserRepository.userMap.put("4", new User("4", "user4", "phone4"));
     }
 
     @Test
     void createUser() {
-        userRepository.createUser("5", new User("5", "Boba", "141235"));
-        assertEquals(1, UserRepository.userMap.size());
+        userRepository.createUser("5", new User("5", "user5", "phone5"));
+        assertEquals(5, UserRepository.userMap.size());
+        assertTrue(UserRepository.userMap.containsKey("5"));
+    }
+
+    @Test
+    void findUser() {
+        userRepository.findUser("4");
 
     }
 
+    @Test
+    void updateUser() {
+        userRepository.updateUser("2", new User("2", "name6", "phone6"));
+        assertTrue(UserRepository.userMap.containsKey("2"));
+    }
+
+    @Test
+    void deleteUser() {
+        userRepository.deleteUser("3");
+        assertEquals(3, UserRepository.userMap.size());
+        assertFalse(UserRepository.userMap.containsKey("3"));
+    }
 }
