331f1a7eef3a4dbf988f38815f7543627ba3abcb
yurasava <yurasava@gmail.com>
2022-08-14 20:47:11 +0300

    TGBOT-44 correction CustomerProviderTest after CustomerProvider refactoring

--- stoservice/src/test/java/com/tutrit/stoservice/provider/CustomerProviderTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/provider/CustomerProviderTest.java
@@ -8,7 +8,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class CustomerProviderTest {
 
@@ -23,25 +22,13 @@ class CustomerProviderTest {
     void getCustomers() {
         Set<Customer> customerSet = new HashSet<>();
 
+        customerSet.add(new Customer("id0", "name0", "city0", "phoneNumber0", "email0"));
         customerSet.add(new Customer("id1", "name1", "city1", "phoneNumber1", "email1"));
         customerSet.add(new Customer("id2", "name2", "city2", "phoneNumber2", "email2"));
         customerSet.add(new Customer("id3", "name3", "city3", "phoneNumber3", "email3"));
         customerSet.add(new Customer("id4", "name4", "city4", "phoneNumber4", "email4"));
-        customerSet.add(new Customer("id5", "name5", "city5", "phoneNumber5", "email5"));
 
-        assertEquals(customerSet, customerProvider.getCustomers());
-        assertEquals(5, customerProvider.getCustomers().size());
-    }
-
-    @Test
-    void getInstance() {
-        Customer customer1 = customerProvider.getInstance();
-        Customer customer2 = customerProvider.getInstance();
-        Customer customer3 = customerProvider.getInstance();
-
-        assertEquals(customer1, customer2);
-
-        customer3.setId("id1");
-        assertNotEquals(customer1, customer3);
+        assertEquals(customerSet, customerProvider.getCustomers(5));
+        assertEquals(5, customerProvider.getCustomers(5).size());
     }
 }
