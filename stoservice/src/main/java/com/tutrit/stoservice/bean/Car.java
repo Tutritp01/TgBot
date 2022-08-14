44ea4268436576a172086a405b0c5d66dfd711c5
Sav4ykVladzislau <44sav4yk@gmail.com>
2022-08-14 09:52:55 +0300

    Create constructor in class Car

--- stoservice/src/main/java/com/tutrit/stoservice/bean/Car.java
+++ stoservice/src/main/java/com/tutrit/stoservice/bean/Car.java
@@ -25,6 +25,9 @@ public class Car {
         this.year = year;
     }
 
+    public Car(String id, String brand, String model, String generation, String modification, String engine, String year) {
+    }
+
     public String getId() {
         return id;
     }
