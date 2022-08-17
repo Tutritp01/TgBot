e15b7bb50856da875ab92650fa491076e3f1a284
lamaconda <kennethoffi6.6@gmail.com>
2022-08-18 02:48:02 +0400

    TGBOT-66 adds user provider

---
+++ stoservice/src/main/java/com/tutrit/stoservice/provider/UserProvider.java
@@ -0,0 +1,35 @@
+package com.tutrit.stoservice.provider;
+
+import com.tutrit.stoservice.bean.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserProvider {
+
+    static Map<String, Object> getOrders() {
+        Map<String, Object> initialUsers = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            User user = getInstance(i);
+            initialUsers.put(user.getId(), user);
+        }
+        return initialUsers;
+    }
+
+    static Map<String, Object> getUsers(int amount) {
+        Map<String, Object> initialUsers = new HashMap<>();
+        for (int i = 0; i < amount; i++) {
+            User user = getInstance(i);
+            initialUsers.put(user.getId(), user);
+        }
+        return initialUsers;
+    }
+
+    static User getInstance(int i) {
+        return new User("Id" + i, "User" + i, "Phone" + i);
+    }
+
+    static User getInstance() {
+        return new User("Id", "User", "Phone");
+    }
+}
