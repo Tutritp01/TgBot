8ff3b089774982ec197413061f0c5a4e769ed422
yurasava <yurasava@gmail.com>
2022-09-01 21:35:44 +0300

    TGBOT-69 refactor due to code transfer to a new class

--- stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
+++ stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
@@ -20,29 +20,7 @@ public class StoserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoserviceApplication.class, args);
 
-        CarProvider carProvider = new CarProvider();
-        CarRepository.cars.addAll(carProvider.getCar());
-
-        CustomerProvider customerProvider = new CustomerProvider();
-        CustomerRepository.customers.addAll(customerProvider.getCustomers(5));
-
-        EngineerRepository engineerRepository = new EngineerRepository();
-        Engineer[] engineers = EngineerProvider.getEngineers(5);
-        for (int i = 0; i < 5; i++) {
-            engineerRepository.createEngineer(engineers[i]);
-        }
-
-        OrderRepository orderRepository = new OrderRepository();
-        Map<Object, Object> orders = OrderProvider.getOrders();
-        for (Object order : orders.values()) {
-            orderRepository.createOrder((Order) order);
-        }
-
-        UserRepository userRepository = new UserRepository();
-        Map<String, Object> userMap = UserProvider.getUsers(5);
-        for (Object user : userMap.values()) {
-            userRepository.createUser((User) user);
-        }
+       new DemoDataLoader().run();
 
         log.info("""
                 Command List:
