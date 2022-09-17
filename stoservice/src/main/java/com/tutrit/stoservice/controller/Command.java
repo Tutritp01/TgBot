22f06e19304ee4c8aa00c91850fbbf3dbcaa3ce0
Sav4ykVladzislau <44sav4yk@gmail.com>
2022-09-17 18:56:31 +0300

    Add Help command to Enum Command

--- stoservice/src/main/java/com/tutrit/stoservice/controller/Command.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/Command.java
@@ -1,19 +1,25 @@
 package com.tutrit.stoservice.controller;
 
-public  enum Command {
+import java.util.*;
+
+public enum Command {
 
     SHOW_DATA("show data"),
     EXIT("exit"),
+    HELP("help"),
     NOT_A_COMMAND(null);
     String command;
 
     Command(String command) {
         this.command = command;
     }
-    public static Command fromString (String command) {
-        return switch (command){
+
+
+    public static Command fromString(String command) {
+        return switch (command) {
             case "show data" -> SHOW_DATA;
             case "exit" -> EXIT;
+            case "help" -> HELP;
             default -> NOT_A_COMMAND;
         };
     }
