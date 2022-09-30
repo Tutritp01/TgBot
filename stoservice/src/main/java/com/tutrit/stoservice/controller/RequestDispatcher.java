e09a3fa29948c5a682dbf237259233ce500b1ae3
yurasava <yurasava@gmail.com>
2022-09-30 22:59:15 +0300

    added REGISTER_NEW_USER command to dispatcher

--- stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
@@ -1,12 +1,15 @@
 package com.tutrit.stoservice.controller;
 
 
+import com.tutrit.stoservice.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.tutrit.stoservice.context.ApplicationContext.get;
+
 public class RequestDispatcher {
 
-    public static Logger logger = LoggerFactory.getLogger("Dispatcher");
+    public static final Logger logger = LoggerFactory.getLogger("Dispatcher");
 
     public void doDispatch(String userInput) {
         Request request = new Request(userInput);
@@ -17,7 +20,7 @@ public class RequestDispatcher {
             case EXIT -> new ExitController().doCommand(request, response);
             case NOT_A_COMMAND -> new NotACommandController().doCommand(request, response);
             case HELP -> new HelpController().doCommand(request, response);
-
+            case REGISTER_NEW_USER -> new UserController(get(UserService.class)).doCommand(request, response);
         }
         logger.info(response.getResponse());
     }
