49b058588d3ad4f00bf5b6bb49fdeade75e82791
yurasava <yurasava@gmail.com>
2022-09-30 23:05:39 +0300

    added tests

---
+++ stoservice/src/test/java/com/tutrit/stoservice/mock/UserRepositoryMock.java
@@ -0,0 +1,50 @@
+package com.tutrit.stoservice.mock;
+
+import com.tutrit.stoservice.bean.User;
+import com.tutrit.stoservice.repository.UserRepository;
+
+public class UserRepositoryMock extends UserRepository {
+    @Override
+    public User save(User user) {
+        return user;
+    }
+
+    @Override
+    public void saveAll(Iterable<User> users) {
+    }
+
+    @Override
+    public User find(User user) {
+        return null;
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(String id) {
+        return null;
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String id) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return 0;
+    }
+}
