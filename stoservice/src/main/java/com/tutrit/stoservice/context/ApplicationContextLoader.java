a187f5b3b322b41014e3d84c9312d26cf04bf99d
yurasava <yurasava@gmail.com>
2022-10-01 20:19:58 +0300

    added commandControllerMap to context

--- stoservice/src/main/java/com/tutrit/stoservice/context/ApplicationContextLoader.java
+++ stoservice/src/main/java/com/tutrit/stoservice/context/ApplicationContextLoader.java
@@ -6,6 +6,8 @@ import com.tutrit.stoservice.provider.CustomerProvider;
 import com.tutrit.stoservice.repository.*;
 import com.tutrit.stoservice.service.UserService;
 
+import java.util.Map;
+
 import static com.tutrit.stoservice.context.ApplicationContext.get;
 import static com.tutrit.stoservice.context.ApplicationContext.put;
 
@@ -28,6 +30,14 @@ public class ApplicationContextLoader {
         put(HelpController.class, new HelpController());
         put(NotACommandController.class, new NotACommandController());
         put(UserController.class, new UserController(get(UserService.class)));
+
+        Map<Command, CommandController> commandCommandControllerMap = Map.of(
+                get(DataController.class).getCommand(), get(DataController.class),
+                get(ExitController.class).getCommand(), get(ExitController.class),
+                get(HelpController.class).getCommand(), get(HelpController.class),
+                get(NotACommandController.class).getCommand(), get(NotACommandController.class),
+                get(UserController.class).getCommand(), get(UserController.class)
+        );
 
         put(CarProvider.class, new CarProvider());
         put(CustomerProvider.class, new CustomerProvider());
