c9781bb9f79e46ec5d787bcdbd54a98626471000
yurasava <yurasava@gmail.com>
2022-10-01 21:40:34 +0300

    output check added

--- stoservice/src/test/java/com/tutrit/stoservice/controller/RequestDispatcherTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/controller/RequestDispatcherTest.java
@@ -3,13 +3,25 @@ package com.tutrit.stoservice.controller;
 import com.tutrit.stoservice.context.ApplicationContext;
 import com.tutrit.stoservice.context.ApplicationContextLoader;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class RequestDispatcherTest {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
 
     @BeforeAll
     public static void init() {
         ApplicationContextLoader.run();
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(output));
     }
 
     @Test
@@ -17,5 +29,6 @@ class RequestDispatcherTest {
         String userInput = "new_user";
         RequestDispatcher requestDispatcher = ApplicationContext.get(RequestDispatcher.class);
         requestDispatcher.doDispatch(userInput);
+        assertEquals("[main] INFO Dispatcher - new user has been saved\r\n", output.toString().substring(13));
     }
 }
