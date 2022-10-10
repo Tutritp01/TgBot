cd8f6291541540f9f50c45247a674b081186083c
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2022-10-11 02:09:20 +0300

    Add UtilsInput

---
+++ stoservice/src/main/java/com/tutrit/stoservice/utils/UtilsInput.java
@@ -0,0 +1,35 @@
+package com.tutrit.stoservice.utils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UtilsInput {
+    private UtilsInput() {
+    }
+
+    public static List<String> parseFlags(String userInput) {
+        String[] parseMap = userInput.split("-d");
+        List<String> parseFlags = new ArrayList<>();
+        if (parseMap.length > 1) {
+            parseFlags.add(parseMap[0].trim());
+            parseFlags.add(parseMap[1].trim());
+        } else {
+            parseFlags.add(parseMap[0].trim());
+            parseFlags.add(null);
+        }
+        return parseFlags;
+    }
+
+    public static Map<String, String> stringToMapParser(String toParse) {
+        try {
+            final String[] keysAndValues = toParse.split("&");
+            return Arrays.stream(keysAndValues)
+                    .collect(Collectors.toMap(
+                            a -> a.substring(0, a.indexOf("=")),
+                            b -> b.substring(b.indexOf("=") + 1)
+                    ));
+        }catch (NullPointerException e){
+            return Collections.emptyMap();
+        }
+    }
+}
