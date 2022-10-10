bedc4b3e7a62de7c121ef412cd7d1b803c380886
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-10 08:55:32 +0300

    TGBOT-101 Rename Engineer Service

--- stoservice/src/main/java/com/tutrit/stoservice/service/EngineerService.java
+++ stoservice/src/main/java/com/tutrit/stoservice/service/EngineerService.java
@@ -3,14 +3,14 @@ package com.tutrit.stoservice.service;
 import com.tutrit.stoservice.bean.Engineer;
 import com.tutrit.stoservice.repository.Repository;
 
-public class CreateEngineerService {
+public class EngineerService {
     Repository repository;
 
-    public CreateEngineerService(final Repository repositoryIn) {
+    public EngineerService(final Repository repositoryIn) {
         this.repository = repositoryIn;
     }
 
-    public void create(Engineer engineer) {
+    public void save(Engineer engineer) {
         repository.save(engineer);
     }
 }
