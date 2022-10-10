d250ada7f174c76a6a84f294544b4e0ae172676d
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-10 11:44:22 +0300

    TGBOT-101 Formatting

--- stoservice/src/main/java/com/tutrit/stoservice/util/InputCommand.java
+++ stoservice/src/main/java/com/tutrit/stoservice/util/InputCommand.java
@@ -6,11 +6,12 @@ import java.util.Map;
 public class InputCommand {
 
 
-    static Map<String, String> inputMsg(String inputMsg){
+    static Map<String, String> inputMsg(String inputMsg) {
         Map<String, String> map = new HashMap<>();
         int indexCommand = inputMsg.indexOf('-');
-        String command1 = inputMsg.substring(0,indexCommand).trim();
+        String command1 = inputMsg.substring(0, indexCommand).trim();
         map.put("command", command1);
         return map;
+
     }
 }
