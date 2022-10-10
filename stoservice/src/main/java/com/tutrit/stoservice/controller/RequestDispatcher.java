cde9f9ec87ebb6e79c1f21c6459851c178b0a16e
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-10 14:06:13 +0300

    TGBOT-101 Edit request Dispatcher

--- stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
@@ -3,8 +3,13 @@ package com.tutrit.stoservice.controller;
 
 import com.tutrit.stoservice.repository.EngineerRepository;
 import com.tutrit.stoservice.service.EngineerService;
+import com.tutrit.stoservice.util.InputCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static com.tutrit.stoservice.util.InputCommand.inputMsg;
 
 public class RequestDispatcher {
 
@@ -14,12 +19,14 @@ public class RequestDispatcher {
         Request request = new Request(userInput);
         Response response = new Response();
 
-        switch (Command.fromString(request.getCommand())) {
-            case SHOW_DATA -> new DataController().doCommand(request, response);
-            case EXIT -> new ExitController().doCommand(request, response);
-            case NOT_A_COMMAND -> new NotACommandController().doCommand(request, response);
-            case HELP -> new HelpController().doCommand(request, response);
-            case NEW_ENGINEER -> new EngineerController(new EngineerService(new EngineerRepository())).doCommand(request, response);
+        Map map = inputMsg(userInput);
+
+        switch (Command.fromString(map.get("command").toString())) {
+            case SHOW_DATA -> new DataController().doCommand(map, response);
+            case EXIT -> new ExitController().doCommand(map, response);
+            case NOT_A_COMMAND -> new NotACommandController().doCommand(map, response);
+            case HELP -> new HelpController().doCommand(map, response);
+            case NEW_ENGINEER -> new EngineerController(new EngineerService(new EngineerRepository())).doCommand(map, response);
         }
         logger.info(response.getResponse());
     }
