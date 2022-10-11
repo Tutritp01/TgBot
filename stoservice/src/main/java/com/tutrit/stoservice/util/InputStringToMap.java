7e7632547506b05dbc5aa32307cd581beb838d64
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-11 10:00:01 +0300

    TGBOT-101 Added a utility class to translate the message to MAP

---
+++ stoservice/src/main/java/com/tutrit/stoservice/util/InputStringToMap.java
@@ -0,0 +1,21 @@
+package com.tutrit.stoservice.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InputStringToMap {
+
+    public static Map<String, String> inputMsg(String inputMsg) {
+        Map<String, String> map = new HashMap<>();
+        if (inputMsg.indexOf('-') > 0) {
+            map.put("command", inputMsg.substring(0, inputMsg.indexOf('-')).trim());
+            map.put("format", inputMsg.substring(inputMsg.indexOf('-') + 1, inputMsg.indexOf('-') + 2));
+            String object = inputMsg.substring(inputMsg.indexOf("-d"));
+            object = object.substring(object.indexOf('{') + 1, object.indexOf('}'));
+            map.put("object", object);
+        } else {
+            map.put("command", inputMsg.trim());
+        }
+        return map;
+    }
+}
