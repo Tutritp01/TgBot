eb7e83bb83eb070a71a9b6d4f223379fe8468220
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-13 04:03:04 +0300

    TGBOT-101 Create EngineerControllerTest

---
+++ stoservice/src/test/java/com/tutrit/stoservice/controller/EngineerControllerTest.java
@@ -0,0 +1,16 @@
+package com.tutrit.stoservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EngineerControllerTest {
+
+    @Test
+    void getCommand() {
+    }
+
+    @Test
+    void doCommand() {
+    }
+}
