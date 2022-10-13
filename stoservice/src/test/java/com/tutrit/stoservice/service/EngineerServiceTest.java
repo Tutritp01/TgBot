1c8e7c5bd658fe4fd6f21d4743e1d67d09b809cc
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-13 13:45:09 +0300

    TGBOT-101 Add EngineerServiceTest

---
+++ stoservice/src/test/java/com/tutrit/stoservice/service/EngineerServiceTest.java
@@ -0,0 +1,12 @@
+package com.tutrit.stoservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EngineerServiceTest {
+
+    @Test
+    void save() {
+    }
+}
