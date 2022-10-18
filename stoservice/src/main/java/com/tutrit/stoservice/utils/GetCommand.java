1c323a7c6e6bc6712e30cee9ce2dbdcf16fbd9ff
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2022-10-18 23:23:17 +0300

    refactor

--- stoservice/src/main/java/com/tutrit/stoservice/utils/GetCommand.java
+++ stoservice/src/main/java/com/tutrit/stoservice/utils/GetCommand.java
@@ -3,9 +3,12 @@ package com.tutrit.stoservice.utils;
 public class GetCommand {
     public static String getCommand(String inputMsg) {
         if (inputMsg.indexOf('-') > 0) {
-            return inputMsg.substring(0, inputMsg.indexOf('-')).trim();
+            return inputMsg.substring(0, inputMsg.indexOf('-')).strip();
         } else {
-            return inputMsg.trim();
+            return inputMsg.strip();
         }
+    }
+
+    private GetCommand() {
     }
 }
