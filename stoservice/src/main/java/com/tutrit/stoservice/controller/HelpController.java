035fdcdb7102d26563e0eb07fafbff0f6a8f1c8c
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-18 10:55:25 +0300

    Fix HelpController

--- stoservice/src/main/java/com/tutrit/stoservice/controller/HelpController.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/HelpController.java
@@ -1,12 +1,16 @@
 package com.tutrit.stoservice.controller;
 
+import java.security.PrivateKey;
 import java.util.Arrays;
 
 public class HelpController implements CommandController {
     private static final Command command = Command.HELP;
+    private final String ANY = "Any wrong input will return to the beginning";
 
     public void doCommand(Request request, Response response) {
-        response.setResponse(String.format(Arrays.toString(Command.values()), request.getCommand()));
+        response.setResponse(Arrays.toString(Arrays.stream(Command.values())
+                .map(e -> e.command != null ? e.command : ANY)
+                .toArray()));
     }
 
     @Override
