bd7a6e52c6711b6d6dde3652c8eb0b4afe716847
Vladimir Schneider <schneider_v_s@mail.ru>
2022-10-30 08:44:00 +0300

    TGBOT-101 Create EngineerServiceTest

---
+++ stoservice/src/test/java/com/tutrit/stoservice/service/EngineerServiceTest.java
@@ -0,0 +1,55 @@
+package com.tutrit.stoservice.service;
+
+import com.tutrit.stoservice.bean.Engineer;
+import com.tutrit.stoservice.repository.EngineerRepository;
+import com.tutrit.stoservice.service.mock.EngineerRepositoryMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EngineerServiceTest {
+    EngineerRepository engineerRepository = new EngineerRepositoryMock();
+    EngineerService engineerService = new EngineerService(engineerRepository);
+    Engineer engineerExpected = new Engineer(
+            "1",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            6,
+            7
+    );
+    Engineer engineer = new Engineer(
+            "1",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            6,
+            7
+    );
+    Engineer engineerNoId = new Engineer(
+            "",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            6,
+            7
+    );
+
+    @Test
+    void saveTest() {
+        engineerService.save(engineer);
+        assertEquals(engineerExpected, engineerRepository.engineers[0]);
+    }
+
+    @Test
+    void saveEngineerNoIdTest() {
+        engineerService.save(engineerNoId);
+        assertEquals(engineerExpected, engineerRepository.engineers[0]);
+    }
+}
