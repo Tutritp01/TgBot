3fa6f53ab8abcfb11de0cfea99a4f3a121a827be
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2022-11-03 00:38:16 +0300

    Set dependensi (repository) by constructor and move creation to application conteht loader

--- stoservice/src/main/java/com/tutrit/stoservice/controller/DataController.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/DataController.java
@@ -5,17 +5,30 @@ import com.tutrit.stoservice.repository.*;
 public class DataController implements CommandController {
     private static final Command command = Command.SHOW_DATA;
 
+    CarRepository carRepository;
+    CustomerRepository customerRepository;
+    OrderRepository orderRepository;
+    UserRepository userRepository;
+    EngineerRepository engineerRepository;
+
+    public DataController(final CarRepository carRepository,
+                          final CustomerRepository customerRepository,
+                          final OrderRepository orderRepository,
+                          final UserRepository userRepository,
+                          final EngineerRepository engineerRepository) {
+        this.carRepository = carRepository;
+        this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
+        this.userRepository = userRepository;
+        this.engineerRepository = engineerRepository;
+    }
+
     public void doCommand(Request request, Response response) {
         response.setResponse(String.format(this.loadedDataAsString()));
     }
 
     private String loadedDataAsString() {
         StringBuilder rs = new StringBuilder();
-        CarRepository carRepository = new CarRepository();
-        CustomerRepository customerRepository = new CustomerRepository();
-        OrderRepository orderRepository = new OrderRepository();
-        UserRepository userRepository = new UserRepository();
-        EngineerRepository engineerRepository = new EngineerRepository();
 
         carRepository.findAll().forEach(car -> rs.append(car.toString()));
         customerRepository.findAll().forEach(customer -> rs.append(customer.toString()));
