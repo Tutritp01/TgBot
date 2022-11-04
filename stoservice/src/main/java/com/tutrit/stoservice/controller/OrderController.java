7ee8d650e28e79b182d239ff771ad381a9b243bd
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-04 14:46:03 +0300

    TGBOT-102 Create OrderController

---
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.tutrit.stoservice.controller;
+
+import com.tutrit.stoservice.bean.Order;
+import com.tutrit.stoservice.service.OrderService;
+
+import static com.tutrit.stoservice.utils.GetOrder.getOrder;
+
+public class OrderController implements CommandController {
+    private static final Command command = Command.NEW_ORDER;
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @Override
+    public void doCommand(Request request, Response response) {
+        Order order = getOrder(request.getCommand());
+        orderService.saveOrder(order);
+        response.setResponse("Order " + order.getId() + " is created");
+    }
+
+    @Override
+    public Command getCommand() {
+        return command;
+    }
+}
