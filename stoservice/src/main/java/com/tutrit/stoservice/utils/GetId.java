9c637801172c7032064dc4a641103fce2fc2bd9d
Sav4ykVladzislau <44sav4yk@gmail.com>
2022-11-04 23:54:26 +0300

    This class should pass the id to the method "getCustomer"

--- stoservice/src/main/java/com/tutrit/stoservice/utils/GetId.java
+++ stoservice/src/main/java/com/tutrit/stoservice/utils/GetId.java
@@ -4,7 +4,6 @@ public class GetId {
     private  String getId(String inputMsg) {
         if (inputMsg.contains("-") && inputMsg.contains("-id")) {
            return inputMsg.substring(inputMsg.indexOf('-') + 1, inputMsg.indexOf("-id")).trim();
-
         }
         return "null";
     }
