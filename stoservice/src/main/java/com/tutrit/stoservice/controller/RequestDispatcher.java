79797236075d615ea2e08f7ab8e8904e31523593
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-06 11:55:08 +0300

    TGBOT-78 Add command GET ENGINEER ID to RequestDispatcher

--- stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
@@ -25,7 +25,7 @@ public class RequestDispatcher {
             case NEW_CUSTOMER -> (get(CustomerController.class)).doCommand(request, response);
             case REGISTER_NEW_USER -> get(UserController.class).doCommand(request, response);
             case SAVE_CAR -> get(CarController.class).doCommand(request,response);
-            case NEW_ENGINEER -> get(EngineerController.class).doCommand(request, response);
+            case NEW_ENGINEER, GET_ENGINEER_ID -> get(EngineerController.class).doCommand(request, response);
             case NEW_ORDER -> get(OrderController.class).doCommand(request, response);
         }
         logger.info(response.getResponse());
