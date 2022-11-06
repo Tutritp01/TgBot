6a66310e88081ee889ee5be0ee6813cad1df9ce0
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-06 11:53:32 +0300

    TGBOT-78 Add command GET ENGINEER ID to Command

--- stoservice/src/main/java/com/tutrit/stoservice/controller/Command.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/Command.java
@@ -8,6 +8,7 @@ public enum Command {
     HELP("help"),
     NEW_CUSTOMER("new customer"),
     NEW_ENGINEER("new engineer"),
+    GET_ENGINEER_ID("get engineer id"),
     REGISTER_NEW_USER("new_user"),
     NEW_ORDER("new order"),
     NOT_A_COMMAND(null);
@@ -25,6 +26,7 @@ public enum Command {
             case "new customer" -> NEW_CUSTOMER;
             case "new_user" -> REGISTER_NEW_USER;
             case "new engineer" -> NEW_ENGINEER;
+            case "get engineer id" -> GET_ENGINEER_ID;
             case "save car" -> SAVE_CAR;
             case "new order" -> NEW_ORDER;
             default -> NOT_A_COMMAND;
