3fc1e045d43893db1c09cf4fb132ef1d400a94ea
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-06 12:50:28 +0300

    TGBOT-78 Add class GetIdFromMap

---
+++ stoservice/src/main/java/com/tutrit/stoservice/utils/GetIdFromMap.java
@@ -0,0 +1,14 @@
+package com.tutrit.stoservice.utils;
+
+import java.util.Map;
+
+public class GetIdFromMap {
+
+    private GetIdFromMap() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String getId (Map<String, String> map) {
+        return map.get("Id");
+    }
+}
