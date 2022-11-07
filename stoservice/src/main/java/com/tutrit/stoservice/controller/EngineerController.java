41e42deadf3bf84e30c1b01f8fa1a63b586e5e17
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-07 15:01:00 +0300

    TGBOT-78 Refactoring EngineerController

--- stoservice/src/main/java/com/tutrit/stoservice/controller/EngineerController.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/EngineerController.java
@@ -3,11 +3,13 @@ package com.tutrit.stoservice.controller;
 import com.tutrit.stoservice.bean.Engineer;
 import com.tutrit.stoservice.service.EngineerService;
 import com.tutrit.stoservice.utils.GetCommand;
+import com.tutrit.stoservice.utils.GetIdFromMap;
+import com.tutrit.stoservice.utils.GetMap;
 
 import static com.tutrit.stoservice.utils.UserInputToEngineer.getEngineer;
 
 public class EngineerController implements CommandController {
-    public static Command command;
+    private Command command;
 
     EngineerService engineerService;
 
@@ -23,21 +25,34 @@ public class EngineerController implements CommandController {
     @Override
     public void doCommand(Request request, Response response) {
         switch (GetCommand.getCommand(request.getCommand())) {
-            case "new engineer" -> {
-                command = Command.NEW_ENGINEER;
-                Engineer engineer = getEngineer(request.getCommand());
-                if (engineer != null) {
-                    engineerService.save(engineer);
-                    response.setResponse("Engineer " + engineer.getIdEngineer() + " is created");
-                    return;
-                }
-                response.setResponse("Engineer not created");
-            }
-            case "get engineer id" -> {
-                command = Command.GET_ENGINEER_ID;
-                String id = getID(request.getCommand());
-                engineerService.findById(request.getCommand);
+            case "new engineer" -> newEngineer(request, response);
+            case "get engineer" -> getEngineerId(request, response);
+            default -> response.setResponse("Failure occurred, command not recognized");
+        }
+    }
+
+    private void newEngineer(Request request, Response response) {
+        command = Command.NEW_ENGINEER;
+        Engineer engineer = getEngineer(request.getCommand());
+        if (engineer != null) {
+            engineerService.save(engineer);
+            response.setResponse("Engineer " + engineer.getIdEngineer() + " is created");
+            return;
+        }
+        response.setResponse("Engineer not created");
+    }
+
+    private void getEngineerId(Request request, Response response) {
+        command = Command.GET_ENGINEER_ID;
+        String id = GetIdFromMap.getId(GetMap.getMap(request.getCommand()));
+        if (id != null) {
+            Engineer engineer = engineerService.findById(id);
+            if (engineer != null) {
+                response.setResponse("Engineer with ID: " + engineer.getIdEngineer() + " found");
+            } else {
+                response.setResponse("error 404: car with " + id + " not found");
             }
         }
+        response.setResponse("Incorrectly entered command, failed to find the ID");
     }
 }
