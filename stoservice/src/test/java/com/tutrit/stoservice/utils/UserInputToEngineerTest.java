978ad4029a9052972ac8853cf9932d3cd40ce4de
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-07 15:17:30 +0300

    TGBOT-78 Refactoring UserInputToEngineerTest

--- stoservice/src/test/java/com/tutrit/stoservice/utils/UserInputToEngineerTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/utils/UserInputToEngineerTest.java
@@ -10,17 +10,17 @@ class UserInputToEngineerTest {
 
     @Test
     void getEngineerKeyTest() {
-        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m -d {LastName:One FirstName:Two Function:Three Category:Four Education:Five Experience:6 GeneralExperience:7}"));
+        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}"));
     }
 
     @Test
     void getEngineerKeySymTest() {
-        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m:& -d {LastName:One&FirstName:Two&Function:Three&Category:Four&Education:Five&Experience:6&GeneralExperience:7}"));
+        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m:& -d {lastName:One&firstName:Two&function:Three&category:Four&education:Five&experience:6&generalExperience:7}"));
     }
 
     @Test
     void getEngineerKeySymITest() {
-        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m:| -d {LastName:One|FirstName:Two|Function:Three|Category:Four|Education:Five|Experience:6|GeneralExperience:7}"));
+        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m:| -d {lastName:One|firstName:Two|function:Three|category:Four|education:Five|experience:6|generalExperience:7}"));
     }
 
     @Test
@@ -30,26 +30,26 @@ class UserInputToEngineerTest {
 
     @Test
     void getEngineerKeyTwo() {
-        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m:&214 -d {LastName:One&FirstName:Two&Function:Three&Category:Four&Education:Five&Experience:6&GeneralExperience:7}"));
+        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -m:&214 -d {lastName:One&firstName:Two&function:Three&category:Four&education:Five&experience:6&generalExperience:7}"));
     }
 
     @Test
     void getEngineerKeyJson() {
-        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -json -d {\"LastName\":\"One\", \"FirstName\":\"Two\", \"Function\":\"Three\", \"Category\":\"Four\", \"Education\":\"Five\", \"Experience\":\"6\", \"GeneralExperience\":\"7\"}"));
+        assertEquals(expected, UserInputToEngineer.getEngineer("new engineer -json -d {\"lastName\":\"One\", \"firstName\":\"Two\", \"function\":\"Three\", \"category\":\"Four\", \"education\":\"Five\", \"experience\":\"6\", \"generalExperience\":\"7\"}"));
     }
 
     @Test
     void getEngineerKeyNotCaseTest() {
-        assertNull(UserInputToEngineer.getEngineer("new engineer -md -d {LN:One FN:Two Fun:Three Cat:Four Edu:Five Exp:6 GExp:7}"));
+        assertNull(UserInputToEngineer.getEngineer("new engineer -md -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}"));
     }
 
     @Test
     void getEngineerKeyNotKeyDTest() {
-        assertNull(UserInputToEngineer.getEngineer("new engineer -m {LN:One FN:Two Fun:Three Cat:Four Edu:Five Exp:6 GExp:7}"));
+        assertNull(UserInputToEngineer.getEngineer("new engineer -m {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}"));
     }
 
     @Test
     void getEngineerBracesTest() {
-        assertNull(UserInputToEngineer.getEngineer("new engineer -m -d LN:One FN:Two Fun:Three Cat:Four Edu:Five Exp:6 GExp:7}"));
+        assertNull(UserInputToEngineer.getEngineer("new engineer -m -d lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}"));
     }
 }
