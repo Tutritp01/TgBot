43865804b871578b873736be8b1951ef22e64deb
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-11 22:44:02 +0300

    TGBOT-77 Added test method findById() to OrderServiceTest. Refactoring OrderRepositoryMock

--- stoservice/src/test/java/com/tutrit/stoservice/service/OrderServiceTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/service/OrderServiceTest.java
@@ -9,17 +9,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
+    OrderService orderService;
     OrderRepository orderRepository;
     Order expected;
 
     @BeforeEach
     void setUp() {
         orderRepository = new OrderRepositoryMock();
+        orderService = new OrderService(orderRepository);
         expected = new Order("1", "Vasil", "lada", "to do");
     }
 
     @Test
     void saveOrder() {
-        assertEquals(expected, orderRepository.save(new Order()));
+        orderService.saveOrder(new Order("1", "Vasil", "lada", "to do"));
+        assertEquals(expected, orderRepository.orders.get("1"));
+    }
+
+    @Test
+    void findById() {
+        assertEquals(expected, orderService.findById("id"));
     }
 }
