390e68f6b079ba9cd157867015882d7fd34d10e7
Sav4ykVladzislau <44sav4yk@gmail.com>
2022-11-12 00:15:06 +0300

    Create case "get user"

--- stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
@@ -23,7 +23,7 @@ public class RequestDispatcher {
             case NOT_A_COMMAND ->get(NotACommandController.class).doCommand(request, response);
             case HELP -> get(HelpController.class).doCommand(request, response);
             case NEW_CUSTOMER -> (get(CustomerController.class)).doCommand(request, response);
-            case REGISTER_NEW_USER -> get(UserController.class).doCommand(request, response);
+            case REGISTER_NEW_USER, GET_USER -> get(UserController.class).doCommand(request, response);
             case SAVE_CAR -> get(CarController.class).doCommand(request,response);
             case NEW_ENGINEER, GET_ENGINEER -> get(EngineerController.class).doCommand(request, response);
             case NEW_ORDER -> get(OrderController.class).doCommand(request, response);
