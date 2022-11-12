f1232a1a54ddc627caabf228a62af7638a2adca1
yurasava <yurasava@gmail.com>
2022-11-12 19:53:20 +0300

    TGBOT-76 reformatted lines

--- stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
+++ stoservice/src/main/java/com/tutrit/stoservice/controller/RequestDispatcher.java
@@ -17,11 +17,11 @@ public class RequestDispatcher {
         switch (Command.fromString(getCommand(userInput))) {
             case SHOW_DATA -> get(DataController.class).doCommand(request, response);
             case EXIT -> get(ExitController.class).doCommand(request, response);
-            case NOT_A_COMMAND ->get(NotACommandController.class).doCommand(request, response);
+            case NOT_A_COMMAND -> get(NotACommandController.class).doCommand(request, response);
             case HELP -> get(HelpController.class).doCommand(request, response);
             case NEW_CUSTOMER -> (get(CustomerController.class)).doCommand(request, response);
             case REGISTER_NEW_USER, REGISTER, NEW_EVENT -> get(UserController.class).doCommand(request, response);
-            case SAVE_CAR, GET_CAR -> get(CarController.class).doCommand(request,response);
+            case SAVE_CAR, GET_CAR -> get(CarController.class).doCommand(request, response);
             case NEW_ENGINEER, GET_ENGINEER -> get(EngineerController.class).doCommand(request, response);
             case NEW_ORDER, GET_ORDER -> get(OrderController.class).doCommand(request, response);
             default -> response.setResponse("Error, command not recognized");
