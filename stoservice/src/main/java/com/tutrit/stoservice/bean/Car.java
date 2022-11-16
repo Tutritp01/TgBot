322f72d65d5234ee72d9b5a720a4650d7467ba26
yurasava <yurasava@gmail.com>
2022-11-16 22:06:40 +0300

    TGBOT-169 added addition fields

--- stoservice/src/main/java/com/tutrit/stoservice/bean/Car.java
+++ stoservice/src/main/java/com/tutrit/stoservice/bean/Car.java
@@ -5,6 +5,9 @@ import java.util.Objects;
 public class Car {
 
     private String id;
+    private String owner;
+    private String vin;
+    private String plateNumber;
     private String brand;
     private String model;
     private String generation;
@@ -15,8 +18,11 @@ public class Car {
     public Car() {
     }
 
-    public Car(String id, String brand, String model, String generation, String modification, String engine, int year) {
+    public Car(String id, String owner, String vin, String plateNumber, String brand, String model, String generation, String modification, String engine, int year) {
         this.id = id;
+        this.owner = owner;
+        this.vin = vin;
+        this.plateNumber = plateNumber;
         this.brand = brand;
         this.model = model;
         this.generation = generation;
@@ -31,6 +37,30 @@ public class Car {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
     }
 
     public String getBrand() {
@@ -86,18 +116,21 @@ public class Car {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return year == car.year && Objects.equals(id, car.id) && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(generation, car.generation) && Objects.equals(modification, car.modification) && Objects.equals(engine, car.engine);
+        return year == car.year && Objects.equals(id, car.id) && Objects.equals(owner, car.owner) && Objects.equals(vin, car.vin) && Objects.equals(plateNumber, car.plateNumber) && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(generation, car.generation) && Objects.equals(modification, car.modification) && Objects.equals(engine, car.engine);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, brand, model, generation, modification, engine, year);
+        return Objects.hash(id, owner, vin, plateNumber, brand, model, generation, modification, engine, year);
     }
 
     @Override
     public String toString() {
         return "Car{" +
                 "id='" + id + '\'' +
+                ", owner='" + owner + '\'' +
+                ", vin='" + vin + '\'' +
+                ", plateNumber='" + plateNumber + '\'' +
                 ", brand='" + brand + '\'' +
                 ", model='" + model + '\'' +
                 ", generation='" + generation + '\'' +
