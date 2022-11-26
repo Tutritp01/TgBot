0849d9478dfe7975603354d8c27c3ba49a6629ea
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-26 17:28:04 +0300

    TGBOT-172 Using context in a class

--- stoservice/src/main/java/com/tutrit/stoservice/provider/DemoDataLoader.java
+++ stoservice/src/main/java/com/tutrit/stoservice/provider/DemoDataLoader.java
@@ -1,29 +1,30 @@
 package com.tutrit.stoservice.provider;
 
 import com.tutrit.stoservice.bean.Engineer;
+import com.tutrit.stoservice.context.ApplicationContext;
 import com.tutrit.stoservice.repository.*;
 
 public class DemoDataLoader {
 
     public void run() {
-        CarProvider carProvider = new CarProvider();
-        CarRepository carRepository = new CarRepository();
+        CarProvider carProvider = ApplicationContext.get(CarProvider.class);
+        CarRepository carRepository = ApplicationContext.get(CarRepository.class);
         carRepository.saveAll(carProvider.getCar());
 
-        CustomerProvider customerProvider = new CustomerProvider();
-        CustomerRepository customerRepository = new CustomerRepository();
+        CustomerProvider customerProvider = ApplicationContext.get(CustomerProvider.class);
+        CustomerRepository customerRepository = ApplicationContext.get(CustomerRepository.class);
         customerRepository.saveAll(customerProvider.getCustomers(5));
 
-        EngineerRepository engineerRepository = new EngineerRepository();
+        EngineerRepository engineerRepository = ApplicationContext.get(EngineerRepository.class);
         Engineer[] engineers = EngineerProvider.getEngineers(5);
         for (int i = 0; i < 5; i++) {
             engineerRepository.save(engineers[i]);
         }
 
-        OrderRepository orderRepository = new OrderRepository();
+        OrderRepository orderRepository = ApplicationContext.get(OrderRepository.class);
         orderRepository.saveAll(OrderProvider.getOrders(5));
 
-        UserRepository userRepository = new UserRepository();
+        UserRepository userRepository = ApplicationContext.get(UserRepository.class);
         userRepository.saveAll(UserProvider.getUsers(5));
 
         MessageProvider.getMessages();
