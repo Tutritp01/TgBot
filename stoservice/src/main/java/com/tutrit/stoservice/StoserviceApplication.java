125b242b64c52460cd51081ae26fded3d4cb649c
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-26 21:30:55 +0300

    TGBOT-162 Added a file with settings and a simple configuration of loading demonstration data

--- stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
+++ stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
@@ -4,20 +4,35 @@ import com.tutrit.stoservice.context.ApplicationContextLoader;
 import com.tutrit.stoservice.provider.DemoDataLoader;
 import com.tutrit.stoservice.provider.SendMessageJob;
 import com.tutrit.stoservice.service.MenuService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 @SpringBootApplication
 public class StoserviceApplication {
-    private static final Logger log = LoggerFactory.getLogger("main");
+    public static final String PATH_TO_PROPERTIES = "stoservice/src/main/resources/config.properties";
+    private static boolean loadData;
 
     public static void main(String[] args) {
         SpringApplication.run(StoserviceApplication.class, args);
+        getProperties();
         ApplicationContextLoader.run();
-        new DemoDataLoader().run();
+        if (loadData) new DemoDataLoader().run();
         new MenuService().start();
         new SendMessageJob().start();
+    }
+
+    private static void getProperties() {
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream(PATH_TO_PROPERTIES)) {
+            properties.load(fileInputStream);
+            String loadDemoProp = properties.getProperty("LoadDemoData");
+            if (loadDemoProp.equals("Yes")) loadData = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
