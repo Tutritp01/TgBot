124f57e827b3b62eb6d76c10f3f1e13fc5feacdc
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-26 22:23:15 +0300

    TGBOT-163 Added a field that allows loading demonstration data.

--- stoservice/src/main/java/com/tutrit/stoservice/settings/Configurations.java
+++ stoservice/src/main/java/com/tutrit/stoservice/settings/Configurations.java
@@ -1,4 +1,9 @@
 package com.tutrit.stoservice.settings;
 
 public class Configurations {
+    private boolean loadDataDemo;
+
+    public boolean isLoadDataDemo() {
+        return loadDataDemo;
+    }
 }
