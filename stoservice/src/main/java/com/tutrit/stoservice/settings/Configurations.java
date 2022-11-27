995cb6724ace08e69e89961d3a9f75f093101701
Vladimir Schneider <schneider_v_s@mail.ru>
2022-11-27 10:24:29 +0300

    TGBOT-163 Added a method to read an external configuration file. When reading the settings, its internal fields are changed.

--- stoservice/src/main/java/com/tutrit/stoservice/settings/Configurations.java
+++ stoservice/src/main/java/com/tutrit/stoservice/settings/Configurations.java
@@ -1,9 +1,29 @@
 package com.tutrit.stoservice.settings;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Configurations {
+    private String pathToProperties = "stoservice/src/main/resources/config.properties";
     private boolean loadDataDemo;
+
+    public void setPathToProperties(String pathToProperties) {
+        this.pathToProperties = pathToProperties;
+    }
 
     public boolean isLoadDataDemo() {
         return loadDataDemo;
+    }
+
+    public void getProperties() {
+        Properties properties = new Properties();
+        try (FileInputStream fileInputStream = new FileInputStream(pathToProperties)) {
+            properties.load(fileInputStream);
+            String loadDemoProp = properties.getProperty("LoadDemoData");
+            if (loadDemoProp.equals("Yes")) loadDataDemo = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
