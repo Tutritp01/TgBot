d1290c34c9b2fbfe91b38f0d2e3b7c6e3638cfe6
Sav4ykVladzislau <44sav4yk@gmail.com>
2022-12-01 23:55:43 +0300

    Add parse body as Map add test

---
+++ stoservice/src/main/java/com/tutrit/stoservice/util/GetBodyAsMap.java
@@ -0,0 +1,37 @@
+package com.tutrit.stoservice.util;
+
+import com.tutrit.stoservice.controller.Request;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class GetBodyAsMap {
+    private static final Logger logger = Logger.getLogger(GetBodyAsMap.class.getName());
+
+    private GetBodyAsMap() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Map<String, String> separateBodyFromCommand(Request request) throws ArrayIndexOutOfBoundsException {
+        String userInput = request.getCommand();
+        int index = userInput.indexOf("-d");
+        String body = userInput.substring(index + 3).strip();
+        return stringToMapParser(body);
+    }
+
+    private static Map<String, String> stringToMapParser(String toParse) {
+        try {
+            final String[] keysAndValues = toParse.split("&");
+            return Arrays.stream(keysAndValues)
+                    .collect(Collectors.toMap(
+                            a -> a.substring(0, a.indexOf("=")),
+                            b -> b.substring(b.indexOf("=") + 1)
+                    ));
+        } catch (NullPointerException e) {
+            return Collections.emptyMap();
+        }
+    }
+}
