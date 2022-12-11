97e5f692463b9bf71e6f86aef7411695a49b3063
Vladimir Schneider <schneider_v_s@mail.ru>
2022-12-11 11:27:20 +0300

    TGBOT-49 Added argument check in main method

--- stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
+++ stoservice/src/main/java/com/tutrit/stoservice/StoserviceApplication.java
@@ -4,6 +4,7 @@ import com.tutrit.stoservice.context.ApplicationContextLoader;
 import com.tutrit.stoservice.provider.DemoDataLoader;
 import com.tutrit.stoservice.provider.SendMessageJob;
 import com.tutrit.stoservice.service.MenuService;
+import com.tutrit.stoservice.settings.ArgsSetup;
 import com.tutrit.stoservice.settings.Configurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +17,7 @@ public class StoserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoserviceApplication.class, args);
         ApplicationContextLoader.run();
-        if (args.length > 0) ArgsSetup.readingFlags (args);
+        if (args.length > 0) get(ArgsSetup.class).readingFlags(args);
         if (get(Configurations.class).isLoadDataDemo()) new DemoDataLoader().run();
         new MenuService().start();
         new SendMessageJob().start();
