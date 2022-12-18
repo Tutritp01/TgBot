ed2319948c28d509a016ed093036b023626f71d8
yurasava <yurasava@gmail.com>
2022-12-18 18:12:55 +0300

    updated ApplicationContextLoader

--- stoservice/src/main/java/com/tutrit/stoservice/context/ApplicationContextLoader.java
+++ stoservice/src/main/java/com/tutrit/stoservice/context/ApplicationContextLoader.java
@@ -8,7 +8,7 @@ import com.tutrit.stoservice.repository.*;
 import com.tutrit.stoservice.service.*;
 import com.tutrit.stoservice.settings.Configurations;
 
-import static com.tutrit.stoservice.context.ApplicationContext.get;
+import static com.tutrit.stoservice.context.ApplicationContext.loadClass;
 import static com.tutrit.stoservice.context.ApplicationContext.put;
 
 public class ApplicationContextLoader {
@@ -28,27 +28,27 @@ public class ApplicationContextLoader {
         put(UserMessageRepository.class, new UserMessageRepository());
         put(PromoRepository.class, new PromoRepository());
 
-        put(UserService.class, new UserService(get(UserRepository.class)));
-        put(EngineerService.class, new EngineerService(get(EngineerRepository.class)));
-        put(CarService.class, new CarService(get(CarRepository.class)));
-        put(CustomerService.class, new CustomerService(get(CustomerRepository.class)));
-        put(OrderService.class, new OrderService(get(OrderRepository.class)));
-        put(MessageService.class, new MessageService(get(MessageRepository.class)));
-        put(PromoService.class, new PromoService(get(PromoRepository.class)));
+        put(UserService.class, new UserService(loadClass(UserRepository.class)));
+        put(EngineerService.class, new EngineerService(loadClass(EngineerRepository.class)));
+        put(CarService.class, new CarService(loadClass(CarRepository.class)));
+        put(CustomerService.class, new CustomerService(loadClass(CustomerRepository.class)));
+        put(OrderService.class, new OrderService(loadClass(OrderRepository.class)));
+        put(MessageService.class, new MessageService(loadClass(MessageRepository.class)));
+        put(PromoService.class, new PromoService(loadClass(PromoRepository.class)));
 
-        put(CustomerController.class, new CustomerController(get(CustomerService.class)));
-        put(UserController.class, new UserController(get(UserService.class),
-                get(MessageService.class),
-                get(PromoService.class)));
-        put(CarController.class, new CarController(get(CarService.class)));
-        put(EngineerController.class, new EngineerController(get(EngineerService.class)));
-        put(OrderController.class, new OrderController(get(OrderService.class)));
+        put(CustomerController.class, new CustomerController(loadClass(CustomerService.class)));
+        put(UserController.class, new UserController(loadClass(UserService.class),
+                loadClass(MessageService.class),
+                loadClass(PromoService.class)));
+        put(CarController.class, new CarController(loadClass(CarService.class)));
+        put(EngineerController.class, new EngineerController(loadClass(EngineerService.class)));
+        put(OrderController.class, new OrderController(loadClass(OrderService.class)));
 
-        put(DataController.class, new DataController(get(CarRepository.class),
-                get(CustomerRepository.class),
-                get(OrderRepository.class),
-                get(UserRepository.class),
-                get(EngineerRepository.class)));
+        put(DataController.class, new DataController(loadClass(CarRepository.class),
+                loadClass(CustomerRepository.class),
+                loadClass(OrderRepository.class),
+                loadClass(UserRepository.class),
+                loadClass(EngineerRepository.class)));
         put(ExitController.class, new ExitController());
         put(HelpController.class, new HelpController());
         put(NotACommandController.class, new NotACommandController());
