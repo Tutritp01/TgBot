6046b130ffe0b4ceccc615276b7608d05b929bef
Vladimir Schneider <schneider_v_s@mail.ru>
2022-12-18 11:03:08 +0300

    TGBOT-217 A servlet was created

---
+++ stoservice/src/main/java/com/tutrit/stoservice/servlets/TableEngineerServlet.java
@@ -0,0 +1,36 @@
+package com.tutrit.stoservice.servlets;
+
+import com.tutrit.stoservice.bean.Engineer;
+import com.tutrit.stoservice.context.ApplicationContext;
+import com.tutrit.stoservice.context.ApplicationContextLoader;
+import com.tutrit.stoservice.repository.EngineerRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "AllEngineers", value = "all-engineer")
+public class TableEngineerServlet extends HttpServlet {
+    static {
+        ApplicationContextLoader.run();
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        if (ApplicationContext.get(EngineerRepository.class).findAll() != null) {
+            List<Engineer> listEngineer = (List<Engineer>) ApplicationContext.get(EngineerRepository.class).findAll();
+            request.setAttribute("listEngineers", listEngineer);
+        }
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/list/all-engineer.jsp");
+        try {
+            requestDispatcher.forward(request, response);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException("Exception in TableEngineerServlet.doGet " + e);
+        }
+    }
+}
