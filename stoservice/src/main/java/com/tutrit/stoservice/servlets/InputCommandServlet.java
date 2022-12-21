9222faded7ed93a91d6dee227c45a0cd0641f192
yurasava <yurasava@gmail.com>
2022-12-21 13:30:02 +0300

    TGBOT-237 created servlet for input field

--- stoservice/src/main/java/com/tutrit/stoservice/servlets/InputCommandServlet.java
+++ stoservice/src/main/java/com/tutrit/stoservice/servlets/InputCommandServlet.java
@@ -1,27 +1,23 @@
 package com.tutrit.stoservice.servlets;
 
-
 import com.tutrit.stoservice.context.ApplicationContext;
 import com.tutrit.stoservice.controller.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 public class InputCommandServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String contextPath = req.getContextPath();
         String inputCommand = req.getParameter("input");
 
-        RequestDispatcher requestDispatcher = ApplicationContext.get(RequestDispatcher.class);
-        requestDispatcher.doDispatch(inputCommand);
+        RequestDispatcher dispatcher = ApplicationContext.get(RequestDispatcher.class);
+        dispatcher.doDispatch(inputCommand);
 
-
-        req.setAttribute("output", requestDispatcher.getResp());
+        req.setAttribute("output", dispatcher.getResp());
         req.getRequestDispatcher(contextPath).forward(req, resp);
     }
 }
