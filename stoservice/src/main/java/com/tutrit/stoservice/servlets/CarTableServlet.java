11d6112214df15a142fac54278b74aab7b400f62
yurasava <yurasava@gmail.com>
2022-12-21 13:29:13 +0300

    TGBOT-237 changed library for HttpServlets

--- stoservice/src/main/java/com/tutrit/stoservice/servlets/CarTableServlet.java
+++ stoservice/src/main/java/com/tutrit/stoservice/servlets/CarTableServlet.java
@@ -1,11 +1,11 @@
 package com.tutrit.stoservice.servlets;
 
 import com.tutrit.stoservice.repository.CarRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.tutrit.stoservice.context.ApplicationContext.get;
