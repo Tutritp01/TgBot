ab146cc369f9ff8d19a7e7c44d4d1c0a92689897
yurasava <yurasava@gmail.com>
2022-12-23 22:31:32 +0300

    TGBOT-237 added POST to car card

--- stoservice/src/main/java/com/tutrit/stoservice/servlets/CarCardServlet.java
+++ stoservice/src/main/java/com/tutrit/stoservice/servlets/CarCardServlet.java
@@ -1,5 +1,7 @@
 package com.tutrit.stoservice.servlets;
 
+import com.tutrit.stoservice.bean.Car;
+import com.tutrit.stoservice.repository.CarRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,9 +9,30 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import static com.tutrit.stoservice.context.ApplicationContext.get;
+
 public class CarCardServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/carCard.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Car car = new Car();
+        car.setId(req.getParameter("id"));
+        car.setOwner(req.getParameter("owner"));
+        car.setVin(req.getParameter("vin"));
+        car.setPlateNumber(req.getParameter("plateNumber"));
+        car.setBrand(req.getParameter("brand"));
+        car.setModel(req.getParameter("model"));
+        car.setGeneration(req.getParameter("generation"));
+        car.setModification(req.getParameter("modification"));
+        car.setEngine(req.getParameter("engine"));
+        car.setYear(Integer.parseInt(req.getParameter("year")));
+        get(CarRepository.class).save(car);
+
         req.getRequestDispatcher("/WEB-INF/view/carCard.jsp").forward(req, resp);
     }
 }
