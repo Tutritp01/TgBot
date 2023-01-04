b655b7a2a1f838006be540b24c284416289e9f1b
yurasava <yurasava@gmail.com>
2023-01-04 23:15:10 +0300

    TGBOT-269 created authentication filter

---
+++ stoservice/src/main/java/com/tutrit/stoservice/servlets/AuthenticationFilter.java
@@ -0,0 +1,11 @@
+package com.tutrit.stoservice.servlets;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    }
+}
