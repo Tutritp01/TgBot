2469f0d6f516f47290ed674ecc394d90f33fd8a7
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-01-06 14:21:07 +0300

    Change EngineerControllerTest

--- stoservice/src/test/java/com/tutrit/stoservice/controller/EngineerControllerTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/controller/EngineerControllerTest.java
@@ -1,8 +1,9 @@
 package com.tutrit.stoservice.controller;
 
 import com.tutrit.stoservice.context.ApplicationContext;
-import com.tutrit.stoservice.context.ApplicationContextLoader;
+import com.tutrit.stoservice.mock.EngineerRepositoryUUIDMock;
 import com.tutrit.stoservice.repository.EngineerRepository;
+import com.tutrit.stoservice.service.EngineerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,18 +11,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EngineerControllerTest {
-    EngineerController engineerController;
+    EngineerRepository engineerRepository = new EngineerRepositoryUUIDMock();
+    EngineerController engineerController = new EngineerController(new EngineerService(new EngineerRepositoryUUIDMock()));
     Command command;
     Request request;
     Response response;
 
     @BeforeEach
     void SetUp() {
-        ApplicationContextLoader.run();
-        engineerController = ApplicationContext.get(EngineerController.class);
         response = new Response();
         Arrays.fill(ApplicationContext.get(EngineerRepository.class).engineers, null);
         EngineerRepository.counted = 0;
@@ -29,13 +29,13 @@ class EngineerControllerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "new engineer -m -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}",
-            "new engineer -m -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}"
+            "new engineer -d id=zero&lastName=One&firstName=Two&function=Three&category=Four&education=Five&experience=6&generalExperience=7",
+            "new engineer -d id=zero&lastName=One&firstName=Two&function=Three&category=Four&education=Five&experience=6&generalExperience=7"
     })
     void doCommandFirstNew(String msgTxt) {
         request = new Request(msgTxt);
         engineerController.doCommand(request, response);
-        assertEquals("Engineer 1 is created", response.getResponse());
+        assertEquals("Engineer spyValues0 is created", response.getResponse());
         getCommandToStringNew();
         getCommandStringNew();
     }
@@ -44,49 +44,49 @@ class EngineerControllerTest {
     void doCommandFailRequestNew() {
         request = new Request("new engineer -d id=zero&lastName=One&firstName=Two&function=Three&category=Four&education=Five&experience=6&generalExperience=7");
         engineerController.doCommand(request, response);
+        getCommandToStringNew();
+        getCommandStringNew();
         request = new Request("get engineer -d id=234532");
         engineerController.doCommand(request, response);
         assertEquals("Error 404: Engineer with 234532 not found", response.getResponse());
-//        getCommandToStringNew();
-//        getCommandStringNew();
     }
 
     @Test
-    void doCommandGetEngineer () {
-        request = new Request("new engineer -m -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}");
+    void doCommandGetEngineer() {
+        request = new Request("new engineer -d id=zero&lastName=One&firstName=Two&function=Three&category=Four&education=Five&experience=6&generalExperience=7");
         engineerController.doCommand(request, response);
         engineerController.doCommand(request, response);
-        request = new Request("get engineer -m -d {id:1}");
+        request = new Request("get engineer -m -d id=spyValues0");
         engineerController.doCommand(request, response);
-        assertEquals("Engineer with ID: 1 found", response.getResponse());
-        request = new Request("get engineer -m -d {id:2}");
+        assertEquals("Engineer with ID: spyValues0 found", response.getResponse());
+        request = new Request("get engineer -m -d id=spyValues1");
         engineerController.doCommand(request, response);
-        assertEquals("Engineer with ID: 2 found", response.getResponse());
+        assertEquals("Engineer with ID: spyValues1 found", response.getResponse());
         getCommandToStringGet();
         getCommandStringGet();
     }
 
     @Test
     void doCommandGetEngineerNoId() {
-        request = new Request("new engineer -m -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}");
+        request = new Request("new engineer -d id=zero&lastName=One&firstName=Two&function=Three&category=Four&education=Five&experience=6&generalExperience=7");
         engineerController.doCommand(request, response);
         engineerController.doCommand(request, response);
-        request = new Request("get engineer -m -d {id:}");
+        request = new Request("get engineer -m -d id=123332");
         engineerController.doCommand(request, response);
-        assertEquals("Incorrectly entered command, failed to find the ID", response.getResponse());
+        assertEquals("Error 404: Engineer with 123332 not found", response.getResponse());
     }
 
     @Test
     void doCommandGetEngineerNotFound() {
-        request = new Request("new engineer -m -d {lastName:One firstName:Two function:Three category:Four education:Five experience:6 generalExperience:7}");
+        request = new Request("new engineer -d id=zero&lastName=One&firstName=Two&function=Three&category=Four&education=Five&experience=6&generalExperience=7");
         engineerController.doCommand(request, response);
         engineerController.doCommand(request, response);
-        request = new Request("get engineer -m -d {id:1}");
+        request = new Request("get engineer -d id=spyValues0");
         engineerController.doCommand(request, response);
-        assertEquals("Engineer with ID: 1 found", response.getResponse());
-        request = new Request("get engineer -m -d {id:3}");
+        assertEquals("Engineer with ID: spyValues0 found", response.getResponse());
+        request = new Request("get engineer -d id=spyValues6");
         engineerController.doCommand(request, response);
-        assertEquals("Error 404: Engineer with 3 not found", response.getResponse());
+        assertEquals("Error 404: Engineer with spyValues6 not found", response.getResponse());
 
     }
 
