24012f2b01be88cd814e204ebe15c8ca559405aa
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2023-01-08 00:59:07 +0300

    Add Order transitions and refactor DeleteCarServlet

--- stoservice/src/main/java/com/tutrit/stoservice/servlets/OrderDeleteServlet.java
+++ stoservice/src/main/java/com/tutrit/stoservice/servlets/OrderDeleteServlet.java
@@ -1,6 +1,7 @@
 package com.tutrit.stoservice.servlets;
 
-import com.tutrit.stoservice.repository.CarRepository;
+import com.tutrit.stoservice.repository.CustomerRepository;
+import com.tutrit.stoservice.repository.OrderRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,18 +11,18 @@ import java.io.IOException;
 
 import static com.tutrit.stoservice.context.ApplicationContext.get;
 
-public class DeleteCardServlet extends HttpServlet {
+public class OrderDeleteServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/view/carDelete.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/view/orderDelete.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = String.valueOf(req.getParameter("id"));
-        get(CarRepository.class).deleteById(id);
+        get(OrderRepository.class).deleteById(id);
+        req.getRequestDispatcher("/WEB-INF/view/orderTable.jsp").forward(req, resp);
 
-        new CarTableServlet().doGet(req, resp);
     }
 }
--- stoservice/src/main/webapp/WEB-INF/view/orderCard.jsp
+++ stoservice/src/main/webapp/WEB-INF/view/orderCard.jsp
@@ -9,8 +9,8 @@
 </head>
 <body>
 <div class="position-absolute top-50 start-50 translate-middle">
-    <a href="/carForm" class="btn btn-lg btn-primary" type="button">Edit</a>
-    <a href="/carDelete" class="btn btn-lg btn-danger" type="button">Delete</a>
+    <a href="/orderForm" class="btn btn-lg btn-primary" type="button">Edit</a>
+    <a href="/orderDelete" class="btn btn-lg btn-danger" type="button">Delete</a>
 </div>
 </body>
 </html>
