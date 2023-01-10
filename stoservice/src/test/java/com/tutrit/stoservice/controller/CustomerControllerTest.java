61a7ba423803cee84a7cb5f083d71959f958ebf0
Sav4ykVladzislau <44sav4yk@gmail.com>
2023-01-10 15:50:26 +0300

    change CustomerControllerTest

--- stoservice/src/test/java/com/tutrit/stoservice/controller/CustomerControllerTest.java
+++ stoservice/src/test/java/com/tutrit/stoservice/controller/CustomerControllerTest.java
@@ -4,7 +4,10 @@ import com.tutrit.stoservice.context.ApplicationContext;
 import com.tutrit.stoservice.context.ApplicationContextLoader;
 import com.tutrit.stoservice.mock.CustomerControllerSpy;
 import com.tutrit.stoservice.mock.CustomerRepositoryMock;
+import com.tutrit.stoservice.mock.CustomerRepositoryUUIDMock;
 import com.tutrit.stoservice.mock.CustomerServiceMock;
+import com.tutrit.stoservice.repository.CustomerRepository;
+import com.tutrit.stoservice.repository.OrderRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,19 +23,20 @@ class CustomerControllerTest {
     public void setUp() {
         ApplicationContextLoader.run();
         customerController = get(CustomerController.class);
+        ApplicationContext.get(CustomerRepository.class).clean();
     }
 
     @Test
     void doCommandIntegrationTest() {
-
-
-        Request request = new Request("new customer");
+        CustomerRepository customerRepository = new CustomerRepositoryUUIDMock();
+        CustomerServiceMock customerService = new CustomerServiceMock(customerRepository);
+        Request request = new Request("new customer -d name=bob");
         Response response = new Response();
         ApplicationContext.get(CustomerController.class).doCommand(request, response);
 
 
         Response expectedResponse = new Response();
-        expectedResponse.setResponse("Customer nullis saved");
+        expectedResponse.setResponse("Customer spyValues0is saved");
         Assertions.assertThat(response)
                 .usingRecursiveComparison()
                 .isEqualTo(expectedResponse);
@@ -56,10 +60,12 @@ class CustomerControllerTest {
 
     @Test
     void integrationCarControllerTest() {
+        CustomerRepository customerRepository = new CustomerRepositoryUUIDMock();
+        CustomerServiceMock customerService = new CustomerServiceMock(customerRepository);
         Response response = new Response();
         Request request = new Request("new customer -d id=zero&name=bob&city=minsk&phoneNumber=923429823&email=1823rew@mail.com");
         customerController.doCommand(request, response);
-        assertEquals("Customer zerois saved", response.getResponse());
+        assertEquals("Customer spyValues0is saved", response.getResponse());
         request = new Request("get customer -d id=234532");
         customerController.doCommand(request, response);
         assertEquals("Error 404: customer with 234532 not found", response.getResponse());
