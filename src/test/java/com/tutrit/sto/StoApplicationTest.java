cc3e15647129c6e9879a8c25c74c8a32f9d0b18a
Vladimir Schneider <schneider_v_s@mail.ru>
2023-01-30 15:09:00 +0300

    TGBOT-208 Added Spring Boot and added Lombok, Spring Boot DevTools, Spring Configuration Processor, Spring Web, Config Server, OpenFeign (MAVEN and Gradle)

---
+++ src/test/java/com/tutrit/sto/StoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.tutrit.sto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
